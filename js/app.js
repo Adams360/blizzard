8dee457579a17a137693db298f12b2dee4acb69f
Adam Shaul <adamshaul360@gmail.com>
2020-02-16 00:36:46 +0200

    added lazy loading

--- js/app.js
+++ js/app.js
@@ -32,3 +32,31 @@ function showSlides(n) {
         slides[slideIndex-1].style.display = "block";
         dots[slideIndex-1].className += " active";
     }
+
+
+    const targets = document.querySelectorAll('img');
+
+
+  const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+
+      entries.forEach(entry => {
+
+        if(entry.isIntersecting) {
+          const img = entry.target;
+          const src = img.getAttribute('data-lazy');
+
+          img.setAttribute('src', src);
+          img.classList.add('fade-bottom');
+
+          observer.disconnect();
+        }
+      })
+
+    });
+
+    io.observe(target);
+    
+  };
+
+  targets.forEach(lazyLoad);
